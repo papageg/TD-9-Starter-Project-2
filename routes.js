0d0e581e16c7f6cd6a6852001b2b67428f293467
Greg <greggordon98@gmail.com>
2019-06-19 11:33:54 -0700

    updated
    updated

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
 
 // This array is used to keep track of user records
 // as they are created.
@@ -14,34 +15,42 @@ router.get('/users', (req, res) => {
   res.json(users);
 });
 
+// const nameValidationChain = check('name')
+//   .exists({ checkNull: true, checkFalsy: true })
+//   .withMessage('Please provide a value for "name"');
+
 // Route that creates a new user.
-router.post('/users', (req, res) => {
-  // Get the user from the request body.
-  const user = req.body;
+router.post('/users',[
+  check('name')
+    .exists()
+    .withMessage('Please provide a value for "name"'),
+  check('email')
+    .exists()
+    .withMessage('Please provide a value for "email"'),
+], (req, res) => {
+  // Attempt to get the validation result from the Request object.
+  const errors = validationResult(req);
 
-  const errors = [];
+  //If there are validation errors...
+  if(!errors.isEmpty()) {
+     // Use the Array `map()` method to get a list of error messages.
+    const errorMessages = errors.array().map(error => error.msg);
 
-  //Validate that we have a 'name' value.
-  if(!user.name) {
-    errors.push('Please provide a value for "Name"');
-  }
-
-  //Validate that we have a 'email' value.
-  if(!user.email) {
-    errors.push('Please provide a value for "Email"');
-  }
-
-  // If there are any errors...
-  if (errors.length > 0) {
-  // Return the validation errors to the client.
-    res.status(400).json({ errors });
+    //Return the validation errors to the client.
+    res.status(400).json({ errors: errorMessages });
   } else {
-  // Add the user to the `users` array.
+    //Get the user from the request body. 
+    const user = req.body;
+
+    //Add the user to the 'users' array.
     users.push(user);
 
-  // Set the status to 201 Created and end the response.
-  res.status(201).end();
-}
+    //Set the status to 201 Created and end the response.
+    res.status(201).end();
+  }
+
+  // Get the user from the request body.
+  const user = req.body;
 
   // Add the user to the `users` array.
   users.push(user);
@@ -51,3 +60,36 @@ router.post('/users', (req, res) => {
 });
 
 module.exports = router;
+
+
+
+
+// const errors = [];
+
+//   //Validate that we have a 'name' value.
+//   if(!user.name) {
+//     errors.push('Please provide a value for "Name"');
+//   }
+
+//   //Validate that we have a 'email' value.
+//   if(!user.email) {
+//     errors.push('Please provide a value for "Email"');
+//   }
+
+//   // If there are any errors...
+//   if (errors.length > 0) {
+//   // Return the validation errors to the client.
+//     res.status(400).json({ errors });
+//   } else {
+//   // Add the user to the `users` array.
+//     users.push(user);
+
+//   // Set the status to 201 Created and end the response.
+//   res.status(201).end();
+// }
+
+//   // Add the user to the `users` array.
+//   users.push(user);
+
+//   // Set the status to 201 Created and end the response.
+//   res.status(201).end();
